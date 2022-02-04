8a223509131650914f28dc49b6c28bccda139afa
Maarten Balliauw <maarten@maartenballiauw.be>
2022-02-04 12:00:30 +0100

    UI updates

--- PluginsAndFeatures/azure-toolkit-for-intellij/rider/src/com/microsoft/intellij/configuration/ui/AzureFunctionsConfigurationPanel.kt
+++ PluginsAndFeatures/azure-toolkit-for-intellij/rider/src/com/microsoft/intellij/configuration/ui/AzureFunctionsConfigurationPanel.kt
@@ -24,30 +24,40 @@ package com.microsoft.intellij.configuration.ui
 
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import com.intellij.openapi.ui.BrowseFolderRunnable
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.TextComponentAccessor
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.ui.ColoredTableCellRenderer
+import com.intellij.ui.InsertPathAction
 import com.intellij.ui.SimpleTextAttributes
 import com.intellij.ui.TableSpeedSearch
+import com.intellij.ui.components.fields.ExtendableTextField
 import com.intellij.ui.layout.noGrowY
 import com.intellij.ui.layout.panel
 import com.intellij.ui.table.JBTable
 import com.intellij.ui.table.TableView
 import com.intellij.util.PathUtil
+import com.intellij.util.castSafelyTo
+import com.intellij.util.ui.AbstractTableCellEditor
 import com.intellij.util.ui.ColumnInfo
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.ListTableModel
-import com.intellij.util.ui.LocalPathCellEditor
 import com.microsoft.intellij.configuration.AzureRiderSettings
+import com.microsoft.intellij.ui.extension.getSelectedValue
 import org.jetbrains.plugins.azure.RiderAzureBundle.message
 import java.io.File
 import javax.swing.JPanel
 import javax.swing.JTable
+import javax.swing.JTextField
 import javax.swing.ListSelectionModel
+import javax.swing.plaf.basic.BasicComboBoxEditor
 import javax.swing.table.TableCellEditor
 import javax.swing.table.TableCellRenderer
 
+@Suppress("UnstableApiUsage")
 class AzureFunctionsConfigurationPanel: AzureRiderAbstractConfigurablePanel {
 
     private val properties: PropertiesComponent = PropertiesComponent.getInstance()
@@ -55,6 +65,8 @@ class AzureFunctionsConfigurationPanel: AzureRiderAbstractConfigurablePanel {
     private lateinit var coreToolsEditorModel: ListTableModel<AzureRiderSettings.AzureCoreToolsPathEntry>
     private lateinit var coreToolsEditor: JBTable
     private lateinit var coreToolsDownloadPathEditor: TextFieldWithBrowseButton
+
+    private val isCoreToolsFeedEnabled = Registry.`is`("azure.function_app.core_tools.feed.enabled")
 
     private val coreToolsEditorColumns = arrayOf<ColumnInfo<*, *>>(
             object : ColumnInfo<AzureRiderSettings.AzureCoreToolsPathEntry, String>(message("settings.app_services.function_app.core_tools.configuration.column.functionsVersion")) {
@@ -78,25 +90,95 @@ class AzureFunctionsConfigurationPanel: AzureRiderAbstractConfigurablePanel {
                         override fun customizeCellRenderer(table: JTable, value: Any?, selected: Boolean, hasFocus: Boolean, row: Int, column: Int) {
                             clear()
 
-                            if (item.coreToolsPath.isEmpty()) {
+                            if (isCoreToolsFeedEnabled && item.coreToolsPath.isEmpty()) {
                                 append(message("settings.app_services.function_app.core_tools.configuration.managed_by_ide"), SimpleTextAttributes.GRAY_ATTRIBUTES)
                             } else {
                                 val coreToolsFile = File(item.coreToolsPath)
 
-                                val attributes = if (coreToolsFile.exists() || coreToolsFile.nameWithoutExtension.equals("func", ignoreCase = true))
-                                    SimpleTextAttributes.REGULAR_ATTRIBUTES
-                                else
-                                    SimpleTextAttributes.ERROR_ATTRIBUTES
+                                if (coreToolsFile.nameWithoutExtension.equals("func", ignoreCase = true)) {
+                                    append(message("settings.app_services.function_app.core_tools.configuration.func_from_path"), SimpleTextAttributes.REGULAR_ATTRIBUTES)
+                                } else {
+                                    val attributes = if (coreToolsFile.exists())
+                                        SimpleTextAttributes.REGULAR_ATTRIBUTES
+                                    else
+                                        SimpleTextAttributes.ERROR_ATTRIBUTES
 
-                                append(valueOf(item), attributes)
+                                    append(valueOf(item), attributes)
+                                }
                             }
                         }
                     }
                 }
 
-                override fun getEditor(item: AzureRiderSettings.AzureCoreToolsPathEntry): TableCellEditor? = LocalPathCellEditor()
-                        .fileChooserDescriptor(FileChooserDescriptorFactory.createSingleFolderDescriptor())
-                        .normalizePath(true)
+                override fun getEditor(item: AzureRiderSettings.AzureCoreToolsPathEntry): TableCellEditor? = object : AbstractTableCellEditor() {
+
+                    private val comboBox: ComboBox<CoreToolsComboBoxItem> = ComboBox<CoreToolsComboBoxItem>()
+
+                    init {
+                        val coreToolsPath = File(item.coreToolsPath)
+
+                        // Setup values for editor
+                        if (isCoreToolsFeedEnabled) {
+                            comboBox.addItem(CoreToolsComboBoxItem(message("settings.app_services.function_app.core_tools.configuration.managed_by_ide"), "", true))
+                            if (item.coreToolsPath.isEmpty()) comboBox.selectedIndex = comboBox.itemCount - 1
+                        }
+
+                        comboBox.addItem(CoreToolsComboBoxItem(message("settings.app_services.function_app.core_tools.configuration.func_from_path"), "func", true))
+                        if (coreToolsPath.nameWithoutExtension.equals("func", ignoreCase = true)) comboBox.selectedIndex = comboBox.itemCount - 1
+
+                        if (item.coreToolsPath.isNotEmpty() && !coreToolsPath.nameWithoutExtension.equals("func", ignoreCase = true)) {
+                            comboBox.addItem(CoreToolsComboBoxItem(item.coreToolsPath, item.coreToolsPath, false))
+                            comboBox.selectedIndex = comboBox.itemCount - 1
+                        }
+
+                        // Setup editor
+                        val fileBrowserAccessor = object : TextComponentAccessor<ComboBox<CoreToolsComboBoxItem>> {
+                            override fun getText(component: ComboBox<CoreToolsComboBoxItem>) = component.getSelectedValue()?.value ?: ""
+                            override fun setText(component: ComboBox<CoreToolsComboBoxItem>, text: String) {
+                                val normalizedText = PathUtil.toSystemDependentName(text)
+                                comboBox.addItem(CoreToolsComboBoxItem(normalizedText, normalizedText, false))
+                                comboBox.selectedIndex = comboBox.itemCount - 1
+                            }
+                        }
+                        val selectFolderAction = BrowseFolderRunnable<ComboBox<CoreToolsComboBoxItem>>(
+                                null,
+                                null,
+                                null,
+                                FileChooserDescriptorFactory.createSingleFolderDescriptor(),
+                                comboBox,
+                                fileBrowserAccessor
+                        )
+
+                        comboBox.isEditable = true
+                        comboBox.editor = object : BasicComboBoxEditor() {
+                            override fun createEditorComponent(): JTextField {
+                                val editor = ExtendableTextField()
+                                editor.addBrowseExtension(selectFolderAction, null)
+                                editor.border = null
+                                InsertPathAction.addTo(editor, FileChooserDescriptorFactory.createSingleFolderDescriptor())
+                                return editor
+                            }
+                        }
+                    }
+
+                    override fun getCellEditorValue(): String {
+                        // Allow for manual input
+                        comboBox.editor.item.castSafelyTo<String>()?.let { textEntry ->
+                            if (textEntry.isEmpty()) {
+                                return ""
+                            }
+
+                            val coreToolsPath = File(textEntry)
+                            if (coreToolsPath.exists() || coreToolsPath.nameWithoutExtension.equals("func", ignoreCase = true)) {
+                                return textEntry
+                            }
+                        }
+
+                        return comboBox.getSelectedValue()?.value ?: ""
+                    }
+
+                    override fun getTableCellEditorComponent(table: JTable?, value: Any?, isSelected: Boolean, row: Int, column: Int) = comboBox
+                }
 
                 override fun isCellEditable(item: AzureRiderSettings.AzureCoreToolsPathEntry) = true
             }
@@ -119,7 +201,7 @@ class AzureFunctionsConfigurationPanel: AzureRiderAbstractConfigurablePanel {
             setEnableAntialiasing(true)
             preferredScrollableViewportSize = JBUI.size(200, 100)
 
-            emptyText.setText(message("settings.app_services.function_app.core_tools.configuration.empty_list"))
+            emptyText.text = message("settings.app_services.function_app.core_tools.configuration.empty_list")
 
             TableSpeedSearch(this)
 
@@ -138,6 +220,8 @@ class AzureFunctionsConfigurationPanel: AzureRiderAbstractConfigurablePanel {
                     TextComponentAccessor.TEXT_FIELD_WHOLE_TEXT
             )
 
+            InsertPathAction.addTo(textField, FileChooserDescriptorFactory.createSingleFolderDescriptor())
+
             text = FileUtil.toSystemDependentName(
                     properties.getValue(
                             AzureRiderSettings.PROPERTY_FUNCTIONS_CORETOOLS_DOWNLOAD_PATH,
@@ -152,10 +236,12 @@ class AzureFunctionsConfigurationPanel: AzureRiderAbstractConfigurablePanel {
             }
         }
 
-        row(message("settings.app_services.function_app.core_tools.download_path")) { }
-        row {
-            cell(isFullWidth = true) {
-                component(coreToolsDownloadPathEditor)
+        if (isCoreToolsFeedEnabled) {
+            row(message("settings.app_services.function_app.core_tools.download_path")) { }
+            row {
+                cell(isFullWidth = true) {
+                    component(coreToolsDownloadPathEditor)
+                }
             }
         }
 
@@ -173,5 +259,10 @@ class AzureFunctionsConfigurationPanel: AzureRiderAbstractConfigurablePanel {
                     AzureRiderSettings.PROPERTY_FUNCTIONS_CORETOOLS_DOWNLOAD_PATH,
                     FileUtil.toSystemIndependentName(coreToolsDownloadPathEditor.text))
         }
+    }
+
+    private data class CoreToolsComboBoxItem(val label: String, val value: String, val isPredefinedEntry: Boolean) {
+
+        override fun toString() = label
     }
 }
