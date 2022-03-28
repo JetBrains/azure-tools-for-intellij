4e0b612a486126c7ce5596637d04a9d66b2137b1
Maarten Balliauw <maarten@maartenballiauw.be>
2022-03-28 09:00:57 +0200

    PR review updates

--- PluginsAndFeatures/azure-toolkit-for-intellij/rider/ReSharper.Azure/src/Azure.Intellisense/FunctionApp/ProjectTemplates/DotNetExtensions/Parameters/AzureFunctionsVersionParameterProvider.cs
+++ PluginsAndFeatures/azure-toolkit-for-intellij/rider/ReSharper.Azure/src/Azure.Intellisense/FunctionApp/ProjectTemplates/DotNetExtensions/Parameters/AzureFunctionsVersionParameterProvider.cs
@@ -38,19 +38,29 @@ namespace JetBrains.ReSharper.Azure.Intellisense.FunctionApp.ProjectTemplates.Do
                 if (parameter.Choices != null && !parameter.Choices.IsEmpty())
                 {
                     // Use from template if provided
+                    var content = factory.CreateNextParameters(new[] {expander}, index + 1, context);
                     foreach (var parameterOptionFromTemplate in parameter.Choices)
                     {
-                        var content = factory.CreateNextParameters(new[] {expander}, index + 1, context);
-                        options.Add(new RdProjectTemplateGroupOption(parameterOptionFromTemplate.Key, parameterOptionFromTemplate.Value, null, content));
+                        var presentation = !parameterOptionFromTemplate.Value.DisplayName.IsNullOrWhitespace()
+                            ? parameterOptionFromTemplate.Value.DisplayName
+                            : parameterOptionFromTemplate.Value.Description;
+
+                        options.Add(new RdProjectTemplateGroupOption(parameterOptionFromTemplate.Key, presentation ?? parameterOptionFromTemplate.Key, null, content));
                     }
                 }
                 else
                 {
                     // Use hardcoded list
-                    // REVIEW: Ideally this would be determined based on TFM and Functions runtime worker, but the new project dialog data does not seem to be exposed in this method?
-                    var content = factory.CreateNextParameters(new[] {expander}, index + 1, context);
-                    options.Add(new RdProjectTemplateGroupOption("V4", "V4", null, content));
-                    options.Add(new RdProjectTemplateGroupOption("V3", "V3", null, content));
+                    var isNet6OrHigher = expander.Template.Sdk.Major >= 6 || expander.Template.Sdk.Major == 0; // The Azure Functions templates treat "0" as .NET 6 as well.
+                    var supportedAzureFunctionsVersions = isNet6OrHigher
+                        ? new[] { "V4" }
+                        : new[] { "V3", "V2" };
+                    
+                    foreach (var version in supportedAzureFunctionsVersions)
+                    {
+                        var content = factory.CreateNextParameters(new[] {expander}, index + 1, context);
+                        options.Add(new RdProjectTemplateGroupOption(version, version, null, content));
+                    }
                 }
                 
                 return new RdProjectTemplateGroupParameter(Name,PresentableName, parameter.DefaultValue, Tooltip, options);
