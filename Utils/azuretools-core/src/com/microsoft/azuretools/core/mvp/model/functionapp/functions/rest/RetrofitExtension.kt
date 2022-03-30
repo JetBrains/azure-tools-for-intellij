f9f886737572991a2d02addcc997f06bc6d11fae
Maarten Balliauw <maarten@maartenballiauw.be>
2022-03-30 12:46:07 +0200

    SSLHandshakeException connecting to Azure Cloud Shell #579

--- Utils/azuretools-core/src/com/microsoft/azuretools/core/mvp/model/functionapp/functions/rest/RetrofitExtension.kt
+++ Utils/azuretools-core/src/com/microsoft/azuretools/core/mvp/model/functionapp/functions/rest/RetrofitExtension.kt
@@ -1,18 +1,18 @@
 /**
- * Copyright (c) 2019 JetBrains s.r.o.
- * <p/>
+ * Copyright (c) 2019-2022 JetBrains s.r.o.
+ *
  * All rights reserved.
- * <p/>
+ *
  * MIT License
- * <p/>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
  * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * <p/>
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
  * the Software.
- * <p/>
+ *
  * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
  * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
@@ -23,7 +23,7 @@
 package com.microsoft.azuretools.core.mvp.model.functionapp.functions.rest
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import com.microsoft.azure.AzureResponseBuilder
 import com.microsoft.azure.management.resources.fluentcore.utils.ResourceManagerThrottlingInterceptor
 import com.microsoft.azure.serializer.AzureJacksonAdapter
@@ -34,12 +34,16 @@ import com.microsoft.rest.credentials.ServiceClientCredentials
 import com.microsoft.rest.protocol.Environment
 import com.microsoft.rest.protocol.SerializerAdapter
 import com.microsoft.rest.serializer.JacksonAdapter
+import org.apache.commons.lang3.reflect.FieldUtils
 import java.util.concurrent.TimeUnit
 
 fun <T> AzureManager.getRetrofitClient(environment: Environment, endpoint: Environment.Endpoint, className: Class<T>, tenantId: String): T =
         getRetrofitClient(environment, endpoint, className, RefreshableTokenCredentials(this, tenantId))
 
 fun <T> AzureManager.getRetrofitClient(environment: Environment, endpoint: Environment.Endpoint, className: Class<T>, credentials: ServiceClientCredentials): T =
+        getRetrofitClient(environment, endpoint, className, credentials) { }
+
+fun <T> AzureManager.getRetrofitClient(environment: Environment, endpoint: Environment.Endpoint, className: Class<T>, credentials: ServiceClientCredentials, customizeHttpClientBuilder: (okhttp3.OkHttpClient.Builder) -> Unit): T =
         RestClient.Builder()
                 .withCredentials(credentials)
                 .withBaseUrl(environment, endpoint)
@@ -47,6 +51,12 @@ fun <T> AzureManager.getRetrofitClient(environment: Environment, endpoint: Envir
                 .withSerializerAdapter(KotlinAzureJacksonAdapter())
                 .withInterceptor(ResourceManagerThrottlingInterceptor())
                 .withReadTimeout(180000L, TimeUnit.MILLISECONDS)
+                .apply {
+                    val httpClientBuilder = FieldUtils.getField(this.javaClass, "httpClientBuilder", true).get(this) as? okhttp3.OkHttpClient.Builder
+                    if (httpClientBuilder != null) {
+                        customizeHttpClientBuilder(httpClientBuilder)
+                    }
+                }
                 .build()
                 .retrofit()
                 .create(className)
@@ -56,6 +66,6 @@ fun <T> AzureManager.getRetrofitClient(environment: Environment, endpoint: Envir
  */
 class KotlinAzureJacksonAdapter : JacksonAdapter(), SerializerAdapter<ObjectMapper> {
     init {
-        serializer().registerModule(KotlinModule())
+        serializer().registerModule(kotlinModule())
     }
 }
